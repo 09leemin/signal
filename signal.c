10c39d1dde15ea77590cffb05bcc544745e673f5
mhlee <leemin900627@gmail.com>
2023-01-24 18:04:28 +0900

    signal api - signal api : register signal handler - sigprocmask : masking signal that I want   * sigemptyset : sigset_t set init   * sigaddset : sigset_t set add signal that I want

---
+++ signal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+void sigterm_handler(int signum)
+{
+	// SIGTERM signal handler 
+	printf("got sigterm..\n");
+}
+
+int main(int argc, char **argv)
+{
+	sigset_t set;
+
+	// SIGINT signal blocking
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	sigprocmask(SIG_SETMASK, &set, NULL);
+
+	signal(SIGTERM, sigterm_handler);
+
+	printf("start of while(1)\n");
+	while(1){
+		sleep(1);
+	}
+	return 0;
+}
